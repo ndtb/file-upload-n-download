ab194122fcb95ab3245d3e16a6719bcf37818ca7
Ravi Kumar <ravindtb@gmail.com>
2021-03-09 20:11:13 +0530

    Inital commit

---
+++ src/main/java/org/ravi/FileUploadNDownloadApplication.java
@@ -0,0 +1,13 @@
+package org.ravi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileUploadNDownloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileUploadNDownloadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ravi/FileUploadNDownloadApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ravi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileUploadNDownloadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
